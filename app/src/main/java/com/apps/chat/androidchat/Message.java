559e009ebd839e4fb7fd38f2b5282687b56da6b6
Abhinav <abhi2000.rtk@gmail.com>
2020-07-31 19:16:23 +0530

    Initial commit

---
+++ app/src/main/java/com/apps/chat/androidchat/Message.java
@@ -0,0 +1,32 @@
+package com.apps.chat.androidchat;
+
+
+public class Message {
+
+    private String nickname;
+    private String message ;
+
+    public  Message(){
+
+    }
+    public Message(String nickname, String message) {
+        this.nickname = nickname;
+        this.message = message;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
